7c81860dab67463258e17577b35ed4b4c1f9c527
H1PEZECA <braabozeca@gmail.com>
2025-07-11 09:47:44 -0300

    chore: initialized edit page.tsx

---
+++ app/_components/header.tsx
@@ -0,0 +1,11 @@
+import { Card, CardContent } from "@/app/_components/ui/card"
+
+const Header = () => {
+  return (
+    <Card>
+      <CardContent className="flex flex-col items-center justify-center p-6"></CardContent>
+    </Card>
+  )
+}
+
+export default Header
