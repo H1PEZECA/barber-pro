ed2f4e24cfa47116acea496727f5e5c75dea8ac1
H1PEZECA <braabozeca@gmail.com>
2025-07-11 21:16:04 -0300

    feat: added Header components and Banners

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,10 +1,17 @@
 import { Card, CardContent } from "@/app/_components/ui/card"
+import { MenuIcon } from "lucide-react"
+import Image from "next/image"
+import { Button } from "./ui/button"
 
 const Header = () => {
   return (
     <Card>
-      <CardContent className="flex flex-col items-center justify-center p-6"></CardContent>
-      <h1>Barber Pro</h1>
+      <CardContent className="flex flex-row items-center justify-between p-5">
+        <Image src="/Logo.svg" alt="FSW Barber Logo" height={18} width={120} />
+        <Button size="icon" variant={"outline"}>
+          <MenuIcon />
+        </Button>
+      </CardContent>
     </Card>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,37 @@
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
+import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+
 const HomePage = () => {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">Welcome to Barber Pro!</h1>
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="font-semi-bold text-xl">Olá, Zeca!</h2>
+        <p>Sexta-Feira, 11 de Julho</p>
+
+        <div className="flex items-center gap-1">
+          <Input className="rounded-md" placeholder="Busque um serviço..." />
+          <Button
+            className="rounded-md bg-purple-700 text-white hover:bg-purple-700"
+            variant="outline"
+            size="icon"
+          >
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[190px] w-full">
+          <Image
+            src="/Banner Pizza.svg"
+            fill
+            className="rounded-xl object-cover"
+            alt="Banner Pizza"
+          />
+        </div>
+      </div>
     </div>
   )
 }
