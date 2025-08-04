804d770932dbe8d9412473085ba7dd12af0a8762
H1PEZECA <braabozeca@gmail.com>
2025-08-03 21:12:25 -0300

    chore: added Footer in layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import localFont from "next/font/local"
 import "./globals.css"
 import { Toaster } from "sonner"
+import Footer from "./_components/footer"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         {children}
         <Toaster />
+        <Footer />
       </body>
     </html>
   )
