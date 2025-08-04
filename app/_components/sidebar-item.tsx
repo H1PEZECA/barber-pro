b06b61e20d3b0738d83b4956373c9d2ec4c3bc32
H1PEZECA <braabozeca@gmail.com>
2025-08-04 19:48:20 -0300

    test

--- app/_components/sidebar-item.tsx
+++ app/_components/sidebar-item.tsx
@@ -1,101 +1,104 @@
-import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
-import Image from "next/image"
-import Link from "next/link"
-import { quickSearchOptions } from "../_constants/search"
-import { Button } from "./ui/button"
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "./ui/dialog"
-import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
+"use client"
 
-const SidebarItem = () => {
+import { Button } from "./ui/button"
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
+import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
+import { quickSearchOptions } from "../_constants/search"
+import Link from "next/link"
+import Image from "next/image"
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog"
+import { signOut, useSession } from "next-auth/react"
+import { Avatar, AvatarImage } from "./ui/avatar"
+import SignInDialog from "./sign-in-dialog"
+
+const SidebarSheet = () => {
+  const { data } = useSession()
+  const handleLogoutClick = () => signOut()
+
   return (
     <SheetContent className="overflow-y-auto">
       <SheetHeader>
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
-      {/*User Details*/}
-      <div className="flex items-center justify-between gap-4 border-b border-solid py-5">
-        <h2 className="text-sm font-bold">Olá, Faça seu Login!</h2>
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button size={"icon"}>
-              <LogInIcon />
-            </Button>
-          </DialogTrigger>
-          <DialogContent className="w-[90%] rounded-xl">
-            <DialogHeader>
-              <DialogTitle>Faça Login na Plataforma</DialogTitle>
-              <DialogDescription>
-                Conecte-se com sua conta Google
-              </DialogDescription>
-            </DialogHeader>
-            <Button variant="outline" className="gap-2 font-semibold">
-              <Image
-                src={"/google.svg"}
-                alt="Google"
-                width={16}
-                height={16}
-              ></Image>
-              Google
-            </Button>
-          </DialogContent>
-        </Dialog>
-        {/*<Avatar>
-          <AvatarImage src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-        </Avatar>
-        <div>
-          <p className="text-sm font-bold">John Doe</p>
-          <p className="text-xs text-gray-400">johndoe@gmail.com</p>
-        </div>*/}
+
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        {data?.user ? (
+          <div className="flex items-center gap-2">
+            <Avatar>
+              <AvatarImage src={data?.user?.image ?? ""} />
+            </Avatar>
+
+            <div>
+              <p className="font-bold">{data.user.name}</p>
+              <p className="text-xs">{data.user.email}</p>
+            </div>
+          </div>
+        ) : (
+          <>
+            <h2 className="font-bold">Olá, faça seu login!</h2>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button size="icon">
+                  <LogInIcon />
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="w-[90%]">
+                <SignInDialog />
+              </DialogContent>
+            </Dialog>
+          </>
+        )}
       </div>
-      {/*Menu Items*/}
+
       <div className="flex flex-col gap-2 border-b border-solid py-5">
-        <Button className="justify-start gap-2" variant="ghost" asChild>
-          <SheetClose asChild>
+        <SheetClose asChild>
+          <Button className="justify-start gap-2" variant="ghost" asChild>
             <Link href="/">
               <HomeIcon size={18} />
-              Inicio
+              Início
             </Link>
-          </SheetClose>
-        </Button>
-        <Button className="justify-start gap-2" variant="ghost">
-          <CalendarIcon size={18} />
-          Agendamentos
+          </Button>
+        </SheetClose>
+        <Button className="justify-start gap-2" variant="ghost" asChild>
+          <Link href="/bookings">
+            <CalendarIcon size={18} />
+            Agendamentos
+          </Link>
         </Button>
       </div>
-      {/*Quick Search*/}
+
       <div className="flex flex-col gap-2 border-b border-solid py-5">
         {quickSearchOptions.map((option) => (
-          <Button
-            key={option.title}
-            className="justify-start gap-2"
-            variant="ghost"
-          >
-            <Image
-              src={option.image}
-              height={18}
-              width={18}
-              alt={option.title}
-            />
-            {option.title}
-          </Button>
+          <SheetClose key={option.title} asChild>
+            <Button className="justify-start gap-2" variant="ghost" asChild>
+              <Link href={`/barbershops?service=${option.title}`}>
+                <Image
+                  alt={option.title}
+                  src={option.image}
+                  height={18}
+                  width={18}
+                />
+                {option.title}
+              </Link>
+            </Button>
+          </SheetClose>
         ))}
       </div>
-      {/*Logout*/}
-      <div className="flex flex-col gap-2 py-5">
-        <Button className="justify-start gap-2" variant="ghost">
-          <LogOutIcon size={18} />
-          Sair da conta
-        </Button>
-      </div>
+
+      {data?.user && (
+        <div className="flex flex-col gap-2 py-5">
+          <Button
+            variant="ghost"
+            className="justify-start gap-2"
+            onClick={handleLogoutClick}
+          >
+            <LogOutIcon size={18} />
+            Sair da conta
+          </Button>
+        </div>
+      )}
     </SheetContent>
   )
 }
 
-export default SidebarItem
+export default SidebarSheet
