fc1e1632bd270791602e34edabbb95c256983817
H1PEZECA <braabozeca@gmail.com>
2025-08-04 21:30:21 -0300

    test

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,11 @@
+import { db } from "@/app/_lib/prisma"
 import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { Adapter } from "next-auth/adapters"
 import GoogleProvider from "next-auth/providers/google"
+
 const handler = NextAuth({
+  adapter: PrismaAdapter(db) as Adapter,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
